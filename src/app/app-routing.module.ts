e1afc5e87e898a3d8be5b1167b1c6e95d4740f65
tiket8 <tiket8@tiket8.com>
2024-08-02 14:48:46 -0700

    creacion proyecto no standalone instalacion de bootstrap, se agrego el archivo module y routting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { ElectronicaComponent } from './electronica/electronica.component';
+import { BeterwereComponent } from './beterwere/beterwere.component';
+import { CarritoComponent } from './carrito/carrito.component';
+import { PerfilComponent } from './perfil/perfil.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', component: InicioComponent },
+    { path: 'electronica', component: ElectronicaComponent },
+    { path: 'beterwere', component: BeterwereComponent },
+    { path: 'carrito', component: CarritoComponent },
+    { path: 'perfil', component: PerfilComponent },
+    { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module'; 
 import { AppComponent } from './app.component';
-
+import { InicioComponent } from './inicio/inicio.component';
+import { ElectronicaComponent } from './electronica/electronica.component';
+import { BeterwereComponent } from './beterwere/beterwere.component';
+import { CarritoComponent } from './carrito/carrito.component';
+import { PerfilComponent } from './perfil/perfil.component';
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    ElectronicaComponent,
+    BeterwereComponent,
+    CarritoComponent,
+    PerfilComponent   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
