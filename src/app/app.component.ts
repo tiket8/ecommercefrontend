7ec750ad96c66fc3ebf566ce0994a61d3b638c63
tiket8 <tiket8@tiket8.com>
2024-08-15 18:26:29 -0700

    se agrego el navbar del menu, estilos acorde al dise;o y en modo responsivo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { AuthService } from './auth.service';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'ecommercefrontend';
   constructor(public authService: AuthService) {}
@@ -17,4 +18,11 @@ export class AppComponent {
   isAdmin(): boolean {
     return this.authService.isAdmin();
   }
+   // Propiedad para controlar la visibilidad del menú
+   menuVisible: boolean = false;
+
+   // Método para alternar la visibilidad del menú
+   toggleMenu(): void {
+     this.menuVisible = !this.menuVisible;
+   }
 }
